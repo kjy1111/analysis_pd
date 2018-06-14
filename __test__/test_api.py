f529a79cd12ec0f49dc73a696f1a411f28425cbd
kjy1111 <ekfto7@nate.com>
2018-06-14 17:21:17 +0900

    Initial commit

--- __test__/test_api.py
+++ __test__/test_api.py
@@ -1,8 +1,6 @@
 import analysis_pd.collect.api as pdapi
 
 # test for pd_gen_url
-
-'''
 url = pdapi.pd_gen_url(
     'http://openapi.tour.go.kr/openapi/service/TourismResourceStatsService/getPchrgTrrsrtVisitorList',
     YM='{0:04d}{1:02d}'.format(2017, 1),
@@ -14,18 +12,14 @@ url = pdapi.pd_gen_url(
     pageNo=1)
 
 print(url)
-'''
 
-'''
+
 # test for pd_fetch_foreign_visitor
 item = pdapi.pd_fetch_foreign_visitor(112, 2012, 7)
 print(item)
-'''
-
 
 
 # test for pd_fetch_tourspot_visitor
-
 for year in range(2017, 2017 + 1):
     for month in range(1, 13):
         for item in pdapi.pd_fetch_tourspot_visitor(
@@ -33,4 +27,3 @@ for year in range(2017, 2017 + 1):
                 year=year,
                 month=month):
             print(item)
-
